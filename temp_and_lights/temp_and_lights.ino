7c1031930f645c140f3142bae1a0accfcd62f025
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-10-09 21:00:58 -0400

    moved files into folders. added new FASTLED file

---
+++ temp_and_lights/temp_and_lights.ino
@@ -0,0 +1,138 @@
+/*
+  State change detection (edge detection)
+
+  Often, you don't need to know the state of a digital input all the time, but
+  you just need to know when the input changes from one state to another.
+  For example, you want to know when a button goes from OFF to ON. This is called
+  state change detection, or edge detection.
+
+  This example shows how to detect when a button or button changes from off to on
+  and on to off.
+
+  The circuit:
+  - pushbutton attached to pin 2 from +5V
+  - 10 kilohm resistor attached to pin 2 from ground
+  - LED attached from pin 13 to ground (or use the built-in LED on most
+    Arduino boards)
+
+  created  27 Sep 2005
+  modified 30 Aug 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/ButtonStateChange
+*/
+
+
+/* 
+ * NEOPIXEL BEST PRACTICES for most reliable operation:
+ *- Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
+ *- MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
+ *- NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
+ *- AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
+ *  connect GROUND (-) first, then +, then data.
+ *- When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
+ *  a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
+ * (Skipping these may work OK on your workbench but can fail in the field) 
+ */
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    6
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 50
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+
+// this constant won't change:
+const int  buttonPin = 2;    // the pin that the pushbutton is attached to
+
+
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT_PULLUP);
+  // initialize the LED as an output:
+  //pinMode(ledPin, OUTPUT);
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(100); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == LOW) {
+      // if the current state is HIGH then the button went from off to on:
+      buttonPushCounter++;
+      Serial.println("on");
+      Serial.print("number of button pushes: ");
+      Serial.println(buttonPushCounter);
+    } else {
+      // if the current state is LOW then the button went from on to off:
+      Serial.println("off");
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
+
+
+  // turns on the LED every four button pushes by checking the modulo of the
+  // button push counter. the modulo function gives you the remainder of the
+  // division of two numbers:
+  if (buttonPushCounter % 4 == 0) {
+    colorWipe(strip.Color(255, 255, 255), 10);
+  } else {
+    colorWipe(strip.Color(  0,   0, 255), 10); // Blue
+  }
+
+}
+
+
+
+// Some functions of our own for creating animated effects -----------------
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
