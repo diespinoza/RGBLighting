087ab6b823effaf7af338dc1f0eee04b1843c00d
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-10-09 22:38:08 -0400

    merged DemoReel100 with previous to loop through functions

--- roomlighting/roomlighting.ino
+++ roomlighting/roomlighting.ino
@@ -2,6 +2,7 @@
 
 // How many leds in your strip?
 #define NUM_LEDS 50 
+#define BRIGHTNESS 96
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -18,17 +19,51 @@ void setup() {
 	Serial.begin(57600);
 	Serial.println("resetting");
 	LEDS.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(Typical8mmPixel); //Typical8mmPixel or TypicalPixelString 
-	LEDS.setBrightness(84);
+	LEDS.setBrightness(BRIGHTNESS);
 }
 
-void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, cylon};
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 void loop() { 
+ /* uint8_t hue = 0;
   cylon();
+  delay(1000);
+  rainbow();
+  delay(1000);
+  //hue++;
+*/
+// Call the current pattern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
 
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/120); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
 
   
 }
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void nextPattern(){
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void fadeall(){
+  for(int i = 0; i < NUM_LEDS; i++){
+    leds[i].nscale8(250); 
+    } 
+}
+
 
 void cylon(){
   static uint8_t hue = 0;
@@ -60,4 +95,11 @@ void cylon(){
     delay(10);
   }   
   
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+  FastLED.show();
 }
